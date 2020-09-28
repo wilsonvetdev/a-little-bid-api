f43d3cc23601c30de678d1a4adf84d116be56c93
wilsonng21 <wilsongch92@gmail.com>
2020-09-27 21:39:17 -0400

    new routes and serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
+
   get '/jobs', to: 'jobs#index'
   get '/jobs/:id', to: 'jobs#show'
+  post '/jobs', to: 'jobs#create'
 end
